ecee97f1a5eb3558d5c68b70630e54b5641833e1
Nabila Tabitha <145773910+nabilaatab@users.noreply.github.com>
2026-09-25 19:49:48 +0700

    update_ihsg_streamlit.py

--- ihsg_streamlit.py
+++ ihsg_streamlit.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="IHSG Analytics", layout="wide")
 
 # Header
-st.title("📈 Dashboard Analisis Historis IHSG")
+st.title("Dashboard Analisis Historis IHSG")
 st.markdown("Visualisasi pergerakan Indeks Harga Saham Gabungan (IHSG) berdasarkan dataset historis harian untuk keperluan peninjauan tren pasar.")
 
 # Panggil data
